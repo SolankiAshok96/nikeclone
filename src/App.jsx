8c5316c9d348137af9b94acdd214f8b8323d3400
clear <solankiashok667@gmail.com>
2023-08-21 17:56:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './App.css'
+import {Hero,CustomerReview,Footer,PopularProduct,Services,SpecialOffer,Subscribe,SuperQuality} from './sections'
+import {Nav} from './componets'
+
+
+function App() {
+
+  return (
+    <>
+      <main className='relative'>
+            <Nav/>
+         <section className='xl:padding-l wide:padding-r padding-b'>
+             <Hero/>
+         </section>
+
+         <section className='padding'>
+               <PopularProduct/>
+         </section>
+         <section className='padding'>
+                <SuperQuality/>
+         </section>
+         <section className='padding'>
+                <Services/>
+         </section>
+         <section className='padding'>
+                <SpecialOffer/>
+         </section>
+         <section className='bg-pale-blue  padding'>
+              <CustomerReview/>
+         </section>
+         <section className='padding-x sm:py-32 py-16 w-full'>
+                <Subscribe/>
+         </section>
+         <section className='bg-black padding-x padding-t pb-8'>
+              <Footer/>
+         </section>
+
+        </main>  
+   
+    </>
+  )
+}
+
+export default App
